2b7cace3ed7e4423c63d2bdd68a3ddafdc4b386c
Pedro Henrique <phenriquesz.1000@gmail.com>
2025-01-17 15:12:49 -0300

    criando a contagem regressiva do site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
-console.log("Olá mundo!");
+AOS.init();
+
+const dataDoEvento = new Date("May 03, 2025 20:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diaAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horaAteEvento = Math.floor((distanciaAteEvento % diaEmMs) / horaEmMs);
+    const minutoAteEvento = Math.floor((distanciaAteEvento % horaEmMs) / minutoEmMs);
+    const segundoAteEvento = Math.floor((distanciaAteEvento % minutoEmMs) / 1000);
+
+    console.log(diaAteEvento);
+    console.log(horaAteEvento);
+    console.log(minutoAteEvento);
+    console.log(segundoAteEvento);
+
+    document.getElementById('contador').innerHTML = `${diaAteEvento}d ${horaAteEvento}h ${minutoAteEvento}m ${segundoAteEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
